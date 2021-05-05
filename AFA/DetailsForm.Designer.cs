421e0984ca0c86e94df8c4c8787adf82334ee6b8
bnc464464 <bnc464464@gmail.com>
2021-05-05 12:45:12 +1200

    The first and second pages with no implemented code

---
+++ AFA/DetailsForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace AFA
+{
+    partial class DetailsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DetailsTitleTxb = new System.Windows.Forms.Label();
+            this.NameTxb = new System.Windows.Forms.Label();
+            this.Day1Lbl = new System.Windows.Forms.Label();
+            this.Day2Lbl = new System.Windows.Forms.Label();
+            this.Day3Lbl = new System.Windows.Forms.Label();
+            this.Day4Lbl = new System.Windows.Forms.Label();
+            this.Day5Lbl = new System.Windows.Forms.Label();
+            this.DetailCancelBtn = new System.Windows.Forms.Button();
+            this.DetailsContinueBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Day1Nud = new System.Windows.Forms.NumericUpDown();
+            this.Day2Nud = new System.Windows.Forms.NumericUpDown();
+            this.Day3Nud = new System.Windows.Forms.NumericUpDown();
+            this.Day4Nud = new System.Windows.Forms.NumericUpDown();
+            this.Day5Nud = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.Day1Nud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Day2Nud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Day3Nud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Day4Nud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Day5Nud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DetailsTitleTxb
+            // 
+            this.DetailsTitleTxb.AutoSize = true;
+            this.DetailsTitleTxb.Font = new System.Drawing.Font("Palatino Linotype", 24F);
+            this.DetailsTitleTxb.Location = new System.Drawing.Point(287, 9);
+            this.DetailsTitleTxb.Name = "DetailsTitleTxb";
+            this.DetailsTitleTxb.Size = new System.Drawing.Size(170, 44);
+            this.DetailsTitleTxb.TabIndex = 0;
+            this.DetailsTitleTxb.Text = "Pet Details";
+            // 
+            // NameTxb
+            // 
+            this.NameTxb.AutoSize = true;
+            this.NameTxb.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold);
+            this.NameTxb.Location = new System.Drawing.Point(221, 76);
+            this.NameTxb.Name = "NameTxb";
+            this.NameTxb.Size = new System.Drawing.Size(111, 28);
+            this.NameTxb.TabIndex = 1;
+            this.NameTxb.Text = "Pet Name:";
+            // 
+            // Day1Lbl
+            // 
+            this.Day1Lbl.AutoSize = true;
+            this.Day1Lbl.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Day1Lbl.Location = new System.Drawing.Point(221, 132);
+            this.Day1Lbl.Name = "Day1Lbl";
+            this.Day1Lbl.Size = new System.Drawing.Size(73, 28);
+            this.Day1Lbl.TabIndex = 2;
+            this.Day1Lbl.Text = "Day 1:";
+            // 
+            // Day2Lbl
+            // 
+            this.Day2Lbl.AutoSize = true;
+            this.Day2Lbl.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Day2Lbl.Location = new System.Drawing.Point(221, 174);
+            this.Day2Lbl.Name = "Day2Lbl";
+            this.Day2Lbl.Size = new System.Drawing.Size(73, 28);
+            this.Day2Lbl.TabIndex = 3;
+            this.Day2Lbl.Text = "Day 2:";
+            // 
+            // Day3Lbl
+            // 
+            this.Day3Lbl.AutoSize = true;
+            this.Day3Lbl.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Day3Lbl.Location = new System.Drawing.Point(221, 213);
+            this.Day3Lbl.Name = "Day3Lbl";
+            this.Day3Lbl.Size = new System.Drawing.Size(73, 28);
+            this.Day3Lbl.TabIndex = 4;
+            this.Day3Lbl.Text = "Day 3:";
+            // 
+            // Day4Lbl
+            // 
+            this.Day4Lbl.AutoSize = true;
+            this.Day4Lbl.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Day4Lbl.Location = new System.Drawing.Point(221, 254);
+            this.Day4Lbl.Name = "Day4Lbl";
+            this.Day4Lbl.Size = new System.Drawing.Size(73, 28);
+            this.Day4Lbl.TabIndex = 5;
+            this.Day4Lbl.Text = "Day 4:";
+            // 
+            // Day5Lbl
+            // 
+            this.Day5Lbl.AutoSize = true;
+            this.Day5Lbl.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold);
+            this.Day5Lbl.Location = new System.Drawing.Point(221, 292);
+            this.Day5Lbl.Name = "Day5Lbl";
+            this.Day5Lbl.Size = new System.Drawing.Size(73, 28);
+            this.Day5Lbl.TabIndex = 6;
+            this.Day5Lbl.Text = "Day 5:";
+            // 
+            // DetailCancelBtn
+            // 
+            this.DetailCancelBtn.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold);
+            this.DetailCancelBtn.Location = new System.Drawing.Point(188, 354);
+            this.DetailCancelBtn.Name = "DetailCancelBtn";
+            this.DetailCancelBtn.Size = new System.Drawing.Size(144, 63);
+            this.DetailCancelBtn.TabIndex = 7;
+            this.DetailCancelBtn.Text = "Cancel";
+            this.DetailCancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // DetailsContinueBtn
+            // 
+            this.DetailsContinueBtn.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold);
+            this.DetailsContinueBtn.Location = new System.Drawing.Point(397, 354);
+            this.DetailsContinueBtn.Name = "DetailsContinueBtn";
+            this.DetailsContinueBtn.Size = new System.Drawing.Size(144, 63);
+            this.DetailsContinueBtn.TabIndex = 8;
+            this.DetailsContinueBtn.Text = "Continue";
+            this.DetailsContinueBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox1.Location = new System.Drawing.Point(370, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 32);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Day1Nud
+            // 
+            this.Day1Nud.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Day1Nud.Location = new System.Drawing.Point(370, 132);
+            this.Day1Nud.Name = "Day1Nud";
+            this.Day1Nud.Size = new System.Drawing.Size(171, 30);
+            this.Day1Nud.TabIndex = 10;
+            // 
+            // Day2Nud
+            // 
+            this.Day2Nud.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Day2Nud.Location = new System.Drawing.Point(370, 174);
+            this.Day2Nud.Name = "Day2Nud";
+            this.Day2Nud.Size = new System.Drawing.Size(171, 30);
+            this.Day2Nud.TabIndex = 11;
+            // 
+            // Day3Nud
+            // 
+            this.Day3Nud.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Day3Nud.Location = new System.Drawing.Point(370, 213);
+            this.Day3Nud.Name = "Day3Nud";
+            this.Day3Nud.Size = new System.Drawing.Size(171, 30);
+            this.Day3Nud.TabIndex = 12;
+            // 
+            // Day4Nud
+            // 
+            this.Day4Nud.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Day4Nud.Location = new System.Drawing.Point(370, 254);
+            this.Day4Nud.Name = "Day4Nud";
+            this.Day4Nud.Size = new System.Drawing.Size(171, 30);
+            this.Day4Nud.TabIndex = 13;
+            // 
+            // Day5Nud
+            // 
+            this.Day5Nud.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Day5Nud.Location = new System.Drawing.Point(370, 292);
+            this.Day5Nud.Name = "Day5Nud";
+            this.Day5Nud.Size = new System.Drawing.Size(171, 30);
+            this.Day5Nud.TabIndex = 14;
+            // 
+            // DetailsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Day5Nud);
+            this.Controls.Add(this.Day4Nud);
+            this.Controls.Add(this.Day3Nud);
+            this.Controls.Add(this.Day2Nud);
+            this.Controls.Add(this.Day1Nud);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DetailsContinueBtn);
+            this.Controls.Add(this.DetailCancelBtn);
+            this.Controls.Add(this.Day5Lbl);
+            this.Controls.Add(this.Day4Lbl);
+            this.Controls.Add(this.Day3Lbl);
+            this.Controls.Add(this.Day2Lbl);
+            this.Controls.Add(this.Day1Lbl);
+            this.Controls.Add(this.NameTxb);
+            this.Controls.Add(this.DetailsTitleTxb);
+            this.Name = "DetailsForm";
+            this.Text = "DetailsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.Day1Nud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Day2Nud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Day3Nud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Day4Nud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Day5Nud)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DetailsTitleTxb;
+        private System.Windows.Forms.Label NameTxb;
+        private System.Windows.Forms.Label Day1Lbl;
+        private System.Windows.Forms.Label Day2Lbl;
+        private System.Windows.Forms.Label Day3Lbl;
+        private System.Windows.Forms.Label Day4Lbl;
+        private System.Windows.Forms.Label Day5Lbl;
+        private System.Windows.Forms.Button DetailCancelBtn;
+        private System.Windows.Forms.Button DetailsContinueBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown Day1Nud;
+        private System.Windows.Forms.NumericUpDown Day2Nud;
+        private System.Windows.Forms.NumericUpDown Day3Nud;
+        private System.Windows.Forms.NumericUpDown Day4Nud;
+        private System.Windows.Forms.NumericUpDown Day5Nud;
+    }
+}
--- AFA/Form1.Designer.cs
+++ AFA/Form1.Designer.cs
@@ -29,13 +29,110 @@ namespace AFA
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainTitleTxb = new System.Windows.Forms.Label();
+            this.Infotxb = new System.Windows.Forms.Label();
+            this.CatBtn = new System.Windows.Forms.Button();
+            this.DogBtn = new System.Windows.Forms.Button();
+            this.BirdBtn = new System.Windows.Forms.Button();
+            this.HorseBtn = new System.Windows.Forms.Button();
+            this.CompletedBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MainTitleTxb
+            // 
+            this.MainTitleTxb.AutoSize = true;
+            this.MainTitleTxb.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainTitleTxb.Location = new System.Drawing.Point(200, 9);
+            this.MainTitleTxb.Name = "MainTitleTxb";
+            this.MainTitleTxb.Size = new System.Drawing.Size(378, 44);
+            this.MainTitleTxb.TabIndex = 0;
+            this.MainTitleTxb.Text = "The Animal Feeding App";
+            // 
+            // Infotxb
+            // 
+            this.Infotxb.AutoSize = true;
+            this.Infotxb.Font = new System.Drawing.Font("Palatino Linotype", 14F);
+            this.Infotxb.Location = new System.Drawing.Point(94, 53);
+            this.Infotxb.Name = "Infotxb";
+            this.Infotxb.Size = new System.Drawing.Size(642, 208);
+            this.Infotxb.TabIndex = 1;
+            this.Infotxb.Text = resources.GetString("Infotxb.Text");
+            // 
+            // CatBtn
+            // 
+            this.CatBtn.Location = new System.Drawing.Point(50, 277);
+            this.CatBtn.Name = "CatBtn";
+            this.CatBtn.Size = new System.Drawing.Size(128, 82);
+            this.CatBtn.TabIndex = 2;
+            this.CatBtn.Text = "Cat\r\n*This will be replaced for a pic*";
+            this.CatBtn.UseVisualStyleBackColor = true;
+            // 
+            // DogBtn
+            // 
+            this.DogBtn.Location = new System.Drawing.Point(217, 277);
+            this.DogBtn.Name = "DogBtn";
+            this.DogBtn.Size = new System.Drawing.Size(128, 82);
+            this.DogBtn.TabIndex = 3;
+            this.DogBtn.Text = "Dog\n*This will be replaced for a pic*";
+            this.DogBtn.UseVisualStyleBackColor = true;
+            // 
+            // BirdBtn
+            // 
+            this.BirdBtn.Location = new System.Drawing.Point(384, 277);
+            this.BirdBtn.Name = "BirdBtn";
+            this.BirdBtn.Size = new System.Drawing.Size(128, 82);
+            this.BirdBtn.TabIndex = 4;
+            this.BirdBtn.Text = "Bird\r\n*This will be replaced for a pic*";
+            this.BirdBtn.UseVisualStyleBackColor = true;
+            // 
+            // HorseBtn
+            // 
+            this.HorseBtn.Location = new System.Drawing.Point(549, 277);
+            this.HorseBtn.Name = "HorseBtn";
+            this.HorseBtn.Size = new System.Drawing.Size(128, 82);
+            this.HorseBtn.TabIndex = 5;
+            this.HorseBtn.Text = "Horse\r\n*This will be replaced for a pic*";
+            this.HorseBtn.UseVisualStyleBackColor = true;
+            // 
+            // CompletedBtn
+            // 
+            this.CompletedBtn.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CompletedBtn.Location = new System.Drawing.Point(267, 385);
+            this.CompletedBtn.Name = "CompletedBtn";
+            this.CompletedBtn.Size = new System.Drawing.Size(186, 44);
+            this.CompletedBtn.TabIndex = 6;
+            this.CompletedBtn.Text = "Finished Adding";
+            this.CompletedBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CompletedBtn);
+            this.Controls.Add(this.HorseBtn);
+            this.Controls.Add(this.BirdBtn);
+            this.Controls.Add(this.DogBtn);
+            this.Controls.Add(this.CatBtn);
+            this.Controls.Add(this.Infotxb);
+            this.Controls.Add(this.MainTitleTxb);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label MainTitleTxb;
+        private System.Windows.Forms.Label Infotxb;
+        private System.Windows.Forms.Button CatBtn;
+        private System.Windows.Forms.Button DogBtn;
+        private System.Windows.Forms.Button BirdBtn;
+        private System.Windows.Forms.Button HorseBtn;
+        private System.Windows.Forms.Button CompletedBtn;
     }
 }
 
